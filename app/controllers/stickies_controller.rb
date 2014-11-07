b14269318ec1ecc9aa59c53bc971d1713e9286bb
weidenfreak <meike.wiemann@gmx.de>
2014-11-07 20:26:26 +0100

    wip

--- app/controllers/stickies_controller.rb
+++ app/controllers/stickies_controller.rb
@@ -1,12 +1,14 @@
 # -*- encoding : utf-8 -*-
 class StickiesController < ApplicationController
   def index
+    @stickies = Sticky.all
   end
 
   def new
   end
 
   def create
-    @sticky = Sticky.new params[:sticky]
+    @sticky = Sticky.new params[:sticky].permit(:title, :text)
+    @sticky.save
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@
 HoshiTest::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  root 'stickies#index'
   resources :stickies
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
